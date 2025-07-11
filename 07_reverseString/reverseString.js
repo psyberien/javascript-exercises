0e0da74f6a3a1dcdf661e467159d984ddf0595aa
saak <31166582+psyberien@users.noreply.github.com>
2025-07-12 02:43:13 +0800

    JS7

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    return str
+        .split("")
+        .reverse()
+        .join("");
 };
 
+
+
+console.log(reverseString('hello there'));
 // Do not edit below this line
 module.exports = reverseString;
