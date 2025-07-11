6e95cf4f2aa5795075598de0c45eedce6430530c
saak <31166582+psyberien@users.noreply.github.com>
2025-07-12 02:29:05 +0800

    JS6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if(num < 0)
+        return "ERROR";
+    
+    let res = "";
+    while(num > 0){
+        res += str;
+        num--;
+    }
+    return res;
 };
-
+console.log(repeatString("hey", 3));
 // Do not edit below this line
 module.exports = repeatString;
