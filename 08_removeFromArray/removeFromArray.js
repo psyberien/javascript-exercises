83e22bf51e49295433406d41214a219d5522a01f
saak <31166582+psyberien@users.noreply.github.com>
2025-07-12 03:47:24 +0800

    JS8

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+
+const removeFromArray = function(arr, ...items) {
+    arr = arr.filter((elements) => !items.includes(elements));
+    
+ return arr;
 };
+
+console.log(removeFromArray([5, 4, 2, 2, 1], 2, 3, 6, 5, 4, "lol", '1'));
 
 // Do not edit below this line
 module.exports = removeFromArray;
