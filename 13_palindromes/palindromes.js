24b02bf6e2df38201000d5993dcf535134080ec5
saak <31166582+psyberien@users.noreply.github.com>
2025-07-17 05:55:03 +0800

    JS13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let alpha = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  let word = str
+    .toLowerCase()
+    .split('')
+    .filter(char => alpha.includes(char))
+    .join('');
+  let reverse = word.split('').reverse().join('');
+  return word === reverse;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
