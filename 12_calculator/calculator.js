ec8764985c51c9d67c9c574d5f085d83a10474df
saak <31166582+psyberien@users.noreply.github.com>
2025-07-17 05:24:40 +0800

    JS12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(...args) {
+	let sum = [...args].reduce((a, b) => a + b);
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(...args) {
+	let difference = [...args].reduce((a, b) => a - b);
+  return difference;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+  for (let item of arr) {
+    sum += item;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((num, current) => num * current, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let fact = 1;
+  for (let i = num; i >= 1; i--) {
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
@@ -31,3 +41,4 @@ module.exports = {
   power,
   factorial
 };
+
