836e81251422864abcedac88e44b8c9e45149183
saak <31166582+psyberien@users.noreply.github.com>
2025-07-17 08:15:21 +0800

    JS16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
+function getAge(person){
+  person.yearOfDeath ??= new Date().getFullYear();
+  return person.yearOfDeath - person.yearOfBirth;
+}
 
+const findTheOldest = function(people) {
+ const sorted = people.sort((a,b) => getAge(b) - getAge(a))
+  return sorted[0];
 };
 
 // Do not edit below this line
